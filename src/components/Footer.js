e4a1284657ea1e0058f0fcb4c36cedc33646b992
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-12-18 12:17:22 -0500

    [component] add about and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+       <footer className='footer bd-dark'>
+            <div className='container text-center'>
+                <span>@2820 Built by Ariel Strizower (during COVID)</span>
+            </div>
+       </footer>
+    )
+}
+
+export default Footer;
